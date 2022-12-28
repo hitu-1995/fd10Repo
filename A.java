4ec26a00b15424492bd7be6e739b8f118f1a9583
hitu-1995 <hiteshwankhede11@gmail.com>
2022-12-28 12:58:10 +0530

    v4

--- A.java
+++ A.java
@@ -2,9 +2,6 @@ public class A {
 
     public static void main(String args[]){
     System.out.println("Welcome to git world");
-    System.out.println("Welcome to git world 1");
-    System.out.println("Welcome to git world 2");
-    System.out.println("Welcome to git world 3");
 
    }
 }
