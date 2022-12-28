43f38a5aa11952f93ce69652abc5a2d98d7c98cd
hitu-1995 <hiteshwankhede11@gmail.com>
2022-12-28 12:49:11 +0530

    java file v2

--- A.java
+++ A.java
@@ -3,6 +3,7 @@ public class A {
     public static void main(String args[]){
     System.out.println("Welcome to git world");
     System.out.println("Welcome to git world 1");
+    System.out.println("Welcome to git world 2");
 
    }
 }
